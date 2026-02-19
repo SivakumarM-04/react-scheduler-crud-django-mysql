0ec0963b8ee1921078fbcea321ed8dee26349610
Sivakumar Manimaran <sivakumar.manimaran@syncfusion.com>
2026-02-19 16:19:09 +0530

    Resolve the recurring event issue and update the styles

--- backend/schedulerCrud/views.py
+++ backend/schedulerCrud/views.py
@@ -21,28 +21,24 @@ def UpdateData(request):
             schedule_events_serializer = ScheduleEventsSerializer(data=schedule_events_data)
             if schedule_events_serializer.is_valid():
                 schedule_events_serializer.save()
-                return GetData(request)            
             else:
                 return JsonResponse(schedule_events_serializer.errors, safe=False, status=400)
 
-        elif 'changed' in data and len(data['changed']) > 0:
+        if 'changed' in data and len(data['changed']) > 0:
             for item in data['changed']:
                 event = ScheduleEvents.objects.get(pk=item['Id'])
                 schedule_events_serializer = ScheduleEventsSerializer(event, data=item)
                 if schedule_events_serializer.is_valid():
                     schedule_events_serializer.save()
-                    return GetData(request)
                 else:
                     return JsonResponse(schedule_events_serializer.errors, safe=False, status=400)
-            return JsonResponse("Updated Successfully", safe=False)
 
-        elif 'deleted' in data and len(data['deleted']) > 0:
+        if 'deleted' in data and len(data['deleted']) > 0:
             for item in data['deleted']:
                 event = ScheduleEvents.objects.get(pk=item['Id'])
                 event.delete()
-            return GetData(request)
-        else:
-            return JsonResponse({"error": "No events to add, update or delete"}, status=400)
+        
+        return GetData(request)
+
     else:
         return JsonResponse({"error": "Invalid method"}, status=405)
-    
